bd04645064f963c241a2fbe78d26ab661ec69c15
lovinix <sylph0606@gmail.com>
2019-07-24 15:24:51 +0000

    1196A

---
+++ 1196A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		long long a, b, c;
+		cin >> a >> b >> c;
+		cout << (a + b + c) / 2 << "\n";
+	}
+}
