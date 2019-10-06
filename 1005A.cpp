0dc21a815d0abe116f25ea0a53a5fc754796d2fe
lovinix <sylph0606@gmail.com>
2019-10-06 05:25:30 +0000

    1005A

---
+++ 1005A.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, arr[1010], cnt;
+vector<int> ans;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> arr[i];
+	for (int i = 1; i < N; ++i)
+		if (arr[i] == 1)
+		{
+			++cnt;
+			ans.emplace_back(arr[i - 1]);
+		}
+	ans.emplace_back(arr[N - 1]);
+	++cnt;
+	cout << cnt << "\n";
+	for (auto& i : ans)
+		cout << i << " ";
+}
