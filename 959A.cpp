a2c6a792e8bd890aef9864b960a06721c4509546
lovinix <sylph0606@gmail.com>
2019-12-28 14:15:25 +0000

    959A

---
+++ 959A.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int N;
+	cin >> N;
+	cout << (N % 2 == 0 ? "Mahmoud" : "Ehab");
+}
