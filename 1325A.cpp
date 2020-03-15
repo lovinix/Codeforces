0b534e09611242d4637c568e855e118c4ecc9031
lovinix <sylph0606@gmail.com>
2020-03-15 06:49:04 +0000

    1325A

---
+++ 1325A.cpp
@@ -0,0 +1,18 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, X;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> T;
+	while (T--)
+	{
+		cin >> X;
+		cout << "1 " << X - 1 << "\n";
+	}
+}
