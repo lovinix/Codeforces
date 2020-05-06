61eeb0b45b2bef02ccd9b159efea56f799d39175
lovinix <sylph0606@gmail.com>
2020-05-06 06:36:00 +0000

    1348C

---
+++ 1348C.cpp
@@ -0,0 +1,28 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, N, K;
+string s;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> T;
+	while (T--)
+	{
+		cin >> N >> K;
+		cin >> s;
+		map<char, int> m;
+		for (auto& c : s) m[c]++;
+		string ans;
+		int fl = 0;
+		for (int i = 0; i < 26; ++i)
+		{
+		
+		}
+		cout << ans << "\n";
+	}
+}
